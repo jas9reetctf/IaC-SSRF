980b216262a0a9b0b00120143e959f58502ec916
jas9reetctf <jas9reet.ctf@gmail.com>
2025-07-24 15:34:21 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
 variable "aws_region" {
   default = "us-east-1"
 }
+
+variable "ami" {
+   type        = string
+   description = "Ubuntu 22.04"
+   default     = "ami-0c7217cdde317cfec"
+}
+
+variable "instance_type" {
+   type        = string
+   description = "Instance type"
+   default     = "t2.micro"
+}
